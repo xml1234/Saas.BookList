417e32323855e2dce565cefa95aa5b40ebb6b276
xml <863871937@qq.com>
2018-12-24 19:34:53 +0800

    部分页面代码的完善

--- Yoyosoft.BookList/src/booklist-aspnet-core/src/Yoyosoft.BookList.Core/BookListConsts.cs
+++ Yoyosoft.BookList/src/booklist-aspnet-core/src/Yoyosoft.BookList.Core/BookListConsts.cs
@@ -6,6 +6,6 @@ namespace Yoyosoft.BookList
 
         public const string ConnectionStringName = "Default";
 
-        public const bool MultiTenancyEnabled = true;
+        public const bool MultiTenancyEnabled = false;
     }
 }

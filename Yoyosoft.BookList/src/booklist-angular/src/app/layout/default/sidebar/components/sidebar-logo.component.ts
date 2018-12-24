eebb997c741b0f2e174e8ccfa99815aa8517c59b
xml <863871937@qq.com>
2018-12-24 19:35:23 +0800

    同下

--- Yoyosoft.BookList/src/booklist-angular/src/app/layout/default/sidebar/components/sidebar-logo.component.ts
+++ Yoyosoft.BookList/src/booklist-angular/src/app/layout/default/sidebar/components/sidebar-logo.component.ts
@@ -4,28 +4,29 @@ import { AppComponentBase } from '@shared/component-base/app-component-base';
 import { SettingsService } from '@yoyo/theme';
 
 @Component({
-    templateUrl: './sidebar-logo.component.html',
-    styleUrls: ['./sidebar-logo.component.less'],
-    selector: 'sidebar-logo'
+  templateUrl: './sidebar-logo.component.html',
+  styleUrls: ['./sidebar-logo.component.less'],
+  selector: 'sidebar-logo'
 })
 export class SideBarLogoComponent extends AppComponentBase implements OnInit {
-    isCollapsed: boolean = false;
-    logoBgColor: string;
+  isCollapsed: boolean = false;
+  logoBgColor: string;
 
-    constructor(
-        injector: Injector,
-        public settings: SettingsService
-    ) {
-        super(injector);
-    }
-    ngOnInit() {
-        this.logoBgColor = '#1890ff';
+  constructor(
+    injector: Injector,
+    public settings: SettingsService
+  ) {
+    super(injector);
+  }
+  ngOnInit() {
+    this.logoBgColor = '#1890ff';
+    // this.logoBgColor = '#000';
 
-        abp.event.on('abp.theme-setting.changed', (theme, logoBg) => {
-            this.logoBgColor = logoBg;
-        });
-        abp.event.on('abp.theme-setting.collapsed', collapsed => {
-            this.isCollapsed = collapsed;
-        });
-    }
+    abp.event.on('abp.theme-setting.changed', (theme, logoBg) => {
+      this.logoBgColor = logoBg;
+    });
+    abp.event.on('abp.theme-setting.collapsed', collapsed => {
+      this.isCollapsed = collapsed;
+    });
+  }
 }
